ecdef7a04bfc1c813c3f38ef7080de8788b0e7fa
salem <saleme1987@hotmail.com>
2023-05-09 14:05:50 -0300

    first commit

---
+++ coder-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import ParentComponent from './Components/ParentComponent';
+import { useState } from 'react'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+  const sumar = () => {
+    setCount(count + 1)
+    console.log('Dentro de la función: ',count)
+  }
+  const restar = () => {
+    setCount(count - 1)
+  }
+  console.log('Fuera de la función: ',count)
+  return (
+    <div className="App" >
+      {/* Props */}
+      <h1>Bienvenidos a CH</h1>
+      <ParentComponent />
+
+      {/* Estados */}
+      <button onClick={sumar}>+</button>
+      <h1>{count}</h1>
+      <button onClick={restar}>-</button>
+    </div>
+  );
+}
+
+export default App;
